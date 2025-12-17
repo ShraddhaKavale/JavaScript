ffb5d763a160ede05632436d10a2e1781b17061b
Shraddha <113848597+ShraddhaKavale@users.noreply.github.com>
2025-12-17 20:35:47 +0000

    datatypes intro done

---
+++ 01_Basic/datatypes_demo.js
@@ -0,0 +1,22 @@
+/*
+number - range(2^53)
+Bigint (for bigger no.s eg. in stock market)
+string - '' or ""
+boolean - true/false
+null - standalone value
+undefined - not assigned now maybe can be assigned in future.
+symbol - to find uniqueness
+
+object (imp)
+*/
+
+const dob = 19 
+let fName = "shraddha"
+let nullVal = null;
+let companyName;
+
+console.log(typeof dob) //number
+console.log(typeof fName) //string
+console.log(typeof nullVal) //object
+console.log(typeof companyName) //undefined
+
