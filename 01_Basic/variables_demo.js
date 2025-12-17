be1ba1daea24ac0e897c162b59b0a663b1947ea1
Shraddha <113848597+ShraddhaKavale@users.noreply.github.com>
2025-12-17 19:34:48 +0000

    varibles intro done

---
+++ 01_Basic/variables_demo.js
@@ -0,0 +1,15 @@
+const accountId = 101
+let email = "smk@gmail.com"
+var password = "smk"
+
+accountName = "shraddha"
+let accountInfo;
+// var accountInfo;
+
+// accountId = 999
+console.log(accountId)
+console.log(email)
+console.log(password)
+console.log(accountName)
+
+console.table([accountId,accountName,email,password,accountInfo])
