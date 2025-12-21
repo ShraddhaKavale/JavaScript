88d81965b7e6bcc7710567638d509bc292cfe038
Shraddha <113848597+ShraddhaKavale@users.noreply.github.com>
2025-12-21 09:25:48 +0000

    array demo one

---
+++ 02_Basic/array_demo.js
@@ -0,0 +1,20 @@
+const arr1 = [1,2,3,4]
+// console.log(arr1)
+
+const arr2 = Array(0,1,2,3,4,"smk")
+// console.log(arr2[5])
+
+arr1.push(5)
+console.log(arr1)
+arr1.pop()
+console.log(arr1)
+arr1.unshift(0)
+console.log(arr1)
+arr1.shift()
+console.log(arr1)
+
+console.log(arr1.includes(9));
+console.log(arr1.includes(0));
+
+console.log(arr1.indexOf(5))
+console.log(arr1.indexOf())
